5b61bf02f7c9de4333df0d4207d8479308a163cd
Fabrizio-padula <fabriziopadula80@gmail.com>
2024-05-20 16:24:19 +0200

    feat: adding the hero and organizing files

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="hero"></div>
+    <div class="my-bg">
+        <div class="container">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti vitae voluptates saepe, minus molestias recusandae officia delectus, reprehenderit nemo modi necessitatibus dolorem est quibusdam id totam ut odio quam hic.
+        </div>
+    </div>
+
+@endsection
